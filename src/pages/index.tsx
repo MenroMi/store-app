ff3e01882424dfc8cd0b9ef99fbfc78ab339d6bb
lebedev-023046 <lebedev.023046@gmail.com>
2023-05-09 19:55:34 +0300

    refactor: delete unnecessary imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
 import Layout from '@/components/Layout';
-import styles from '../styles/pageStyles/Home.module.css';
-import TestComponent from '@/components/TestComponent';
-
-import { REGULAR_NAV_LINKS } from '@/constants';
 
 export default function Home() {
   return <Layout title="Shoes Shop | Home Page">hello from main</Layout>;
