f5ebf9863e7620f92259250d4a07df6548cd8399
Cyryl <folkbread@gmail.com>
2023-05-25 12:09:00 +0200

    refactor:  remove useless elements

--- src/components/UI/Cards/CardList/CardList.tsx
+++ src/components/UI/Cards/CardList/CardList.tsx
@@ -1,15 +1,11 @@
 // basic
-import Image, { StaticImageData } from 'next/image';
+import Image from 'next/image';
 import Router, { useRouter } from 'next/router';
-import { useContext } from 'react';
 
 // mui
 import { useTheme, Theme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import { Grid, Box, Typography } from '@mui/material';
-
-// context
-import { FiltersContext } from '@/contexts/filtersContext';
+import { Box, Grid, Typography } from '@mui/material';
 
 // image
 import singInImg from '@/assets/singInBg.png';
@@ -18,25 +14,19 @@ import emptyIcon from '@/assets/icons/empty.svg';
 // component
 import Card from '@/components/UI/Cards/Card/Card';
 import DropDownMenu from '@/components/UI/Menu/DropDownMenu/DropDownMenu';
+import FullScreenLoader from '../../Loader/FullScreenLoader';
 
 // styled component
 import { CardsGridContainer, CatalogIsEmptyContainer, CustomSearchOverlay } from './CardListStyles';
 
 // interface
-import { AttrFromData, ICardListProps } from '@/types/cardListTypes';
+import { AttrFromData } from '@/types/cardListTypes';
 import { getFilteredData } from '@/services/searchApi';
 import { useQuery } from '@tanstack/react-query';
 import { ONE_MOCKED_PRODUCT } from '@/constants';
 
-// interface
-
-// FUNCTIONAL COMPONENT
-const CardList: React.FC<ICardListProps> = ({
-  products = [...new Array(8).fill(ONE_MOCKED_PRODUCT)],
-}): JSX.Element | null => {
+const CardList = () => {
   const theme = useTheme<Theme>();
-  const queryUpMd = useMediaQuery<unknown>(theme.breakpoints.up('md'));
-  const contextFilter = useContext(FiltersContext);
   const router = useRouter();
 
   const { data, isFetched, isError, error, isLoading } = useQuery({
@@ -44,93 +34,74 @@ const CardList: React.FC<ICardListProps> = ({
     queryFn: () => getFilteredData(router.query),
     keepPreviousData: true,
   });
-
-  const isVisible = (elem: JSX.Element, id: number) => {
-    return (
-      <Grid
-        key={id}
-        xl={contextFilter?.hide ? 2.3 : 3}
-        lg={contextFilter?.hide ? 3 : 4}
-        md={contextFilter?.hide ? 4 : 6}
-        sm={5}
-        xs={5.7}
-        item
-      >
-        {elem}
-      </Grid>
-    );
-  };
+  const queryDownMd = useMediaQuery<unknown>(theme.breakpoints.down('md'));
   if (isError) {
     Router.push('/404');
     return null;
   }
 
-  const checkData = () => {
-    if (Array.isArray(data?.data) && data?.data.length > 0) {
-      return data?.data.map(({ id, attributes }: AttrFromData) => {
-        const { name, price, images, gender } = attributes;
-        let url: string | StaticImageData;
-
-        if (images?.data === null || typeof images === 'undefined') {
-          url = singInImg;
-        } else {
-          if (typeof images?.data?.[0]?.attributes?.url === 'undefined') {
-            url = singInImg;
-          } else {
-            url = images?.data?.[0]?.attributes?.url;
-          }
-        }
-
-        return isVisible(
-          <Card
-            productCategory={gender?.data?.id === 3 ? "Men's Shoes" : "Women's Shoes"}
-            productImageSrc={url}
-            productName={name}
-            productPrice={price}
-          >
-            <DropDownMenu productName={name} productID={id} />
-          </Card>,
-          id
-        );
-      });
-    }
-    return (
-      <CatalogIsEmptyContainer>
-        <Box
-          component={Image}
-          src={emptyIcon}
-          alt="catalog is empty"
-          width={200}
-          height={200}
-          sx={{
-            opacity: '0.1',
-          }}
-        />
-        <Typography variant="h2" sx={{ opacity: '0.5' }}>
-          Catalog is empty.
-        </Typography>
-      </CatalogIsEmptyContainer>
-    );
-  };
-
   return (
     <CustomSearchOverlay>
-      <CardsGridContainer
-        container
-        columnSpacing={{
-          md: 5,
-          lg: 5,
-          xl: 7,
-        }}
-        sx={{
-          padding: `${!queryUpMd && '0 0 0 20px'}`,
-          columnGap: contextFilter?.hide && queryUpMd ? { xl: '13px' } : '',
-          rowGap: { md: '32px', xs: '16px' },
-          justifyContent: 'flex-start',
-        }}
-      >
-        {isLoading && !isFetched ? <h1>Loading...</h1> : checkData()}
-      </CardsGridContainer>
+      {isLoading ? (
+        <FullScreenLoader />
+      ) : (
+        <>
+          {Array.isArray(data?.data) ? (
+            <CardsGridContainer container>
+              {data?.data.map(
+                ({
+                  id,
+                  attributes: {
+                    name,
+                    price,
+                    images: { data: imagesData },
+                    gender: { data: genderData },
+                  },
+                }: AttrFromData) => (
+                  <Grid item key={id}>
+                    <Card
+                      productCategory={
+                        genderData ? (genderData.id === 3 ? "Men's Shoes" : "Women's Shoes") : ''
+                      }
+                      productImageSrc={
+                        imagesData
+                          ? imagesData[0]
+                            ? imagesData[0].attributes.url
+                            : singInImg
+                          : singInImg
+                      }
+                      productName={name}
+                      productPrice={price}
+                    >
+                      <DropDownMenu productName={name} productID={id} />
+                    </Card>
+                  </Grid>
+                )
+              )}
+            </CardsGridContainer>
+          ) : (
+            <CatalogIsEmptyContainer>
+              <Box
+                component={Image}
+                src={emptyIcon}
+                alt="catalog is empty"
+                width={queryDownMd ? 150 : 200}
+                height={queryDownMd ? 150 : 200}
+                priority={true}
+                sx={{
+                  opacity: '0.1',
+                }}
+              />
+              <Typography
+                variant="h2"
+                sx={{ opacity: '0.5', width: queryDownMd ? '250px' : '375px' }}
+              >
+                Catalog is empty.
+              </Typography>
+            </CatalogIsEmptyContainer>
+          )}
+        </>
+      )}
     </CustomSearchOverlay>
   );
 };
