07a28b515ce91981a031a21a5cbbdd6e2c8bfe77
lebedev-023046 <lebedev.023046@gmail.com>
2023-05-25 13:35:27 +0300

    feat: add deleteAvatar functionality

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 interface IUpdateData {
   token: string | null;
   id: number;
-  updateFormData: ISettings;
+  dataToUpdate: ISettings;
 }
 
 export const getUser = async (token: string | null) => {
@@ -14,16 +14,26 @@ export const getUser = async (token: string | null) => {
       Authorization: `Bearer ${token}`,
     },
   });
-  console.log(response.data);
+  // console.log(response.data);
   return response.data;
 };
 
-export const updateUser = async ({ token, id, updateFormData }: IUpdateData) => {
-  const response = await axios.put(`${baseURL}users/${id}?populate=*`, updateFormData, {
+export const updateUser = async ({ token, id, dataToUpdate }: IUpdateData) => {
+  const response = await axios.put(`${baseURL}users/${id}?populate=*`, dataToUpdate, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
-  console.log(response.data);
+  // console.log(response.data);
+  return response.data;
+};
+
+export const deleteAvatar = async ({ token, id }: Omit<IUpdateData, 'dataToUpdate'>) => {
+  const response = await axios.delete(`${baseURL}upload/files/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  // console.log(response.data);
   return response.data;
 };
