06dce815569dcfb76a4b371d421c9fd92d48b94d
Cyryl <folkbread@gmail.com>
2023-05-26 11:45:52 +0200

    refactor: changed card list

--- src/components/UI/Cards/CardList/CardList.tsx
+++ src/components/UI/Cards/CardList/CardList.tsx
@@ -26,7 +26,6 @@ import { CardsGridContainer, CatalogIsEmptyContainer, CustomSearchOverlay } from
 import { AttrFromData } from '@/types/cardListTypes';
 import { getFilteredData } from '@/services/searchApi';
 import { useQuery } from '@tanstack/react-query';
-import { ONE_MOCKED_PRODUCT } from '@/constants';
 
 const CardList = () => {
   const theme = useTheme<Theme>();
@@ -51,77 +50,80 @@ const CardList = () => {
         overflowY: `${isFetching && !isLoading ? 'clip' : 'scroll'}`,
       }}
     >
-      <Box
-        sx={{
-          display: `${isFetching && !isLoading ? 'block' : 'none'}`,
-          height: '100%',
-          width: '100%',
-          backgroundColor: 'rgba(255,255, 255, 0.5)',
-          backdropFilter: 'blur(5px)',
-          position: 'absolute',
-          zIndex: '5000',
-          top: '0',
-          transition: '0.3s ease-in',
-        }}
-      >
-        <FullScreenLoader />
-      </Box>
-      <>
-        {Array.isArray(data?.data) && data?.data.length !== 0 ? (
-          <CardsGridContainer container>
-            {data?.data.map(
-              ({
-                id,
-                attributes: {
-                  name,
-                  price,
-                  images: { data: imagesData },
-                  gender: { data: genderData },
-                },
-              }: AttrFromData) => (
-                <Grid item key={id}>
-                  <Card
-                    productCategory={
-                      genderData ? (genderData.id === 3 ? "Men's Shoes" : "Women's Shoes") : ''
-                    }
-                    productImageSrc={
-                      imagesData
-                        ? imagesData[0]
-                          ? imagesData[0].attributes.url
+      {isFetching ? (
+        <Box
+          sx={{
+            // display: `${isFetching && !isLoading ? 'block' : 'none'}`,
+            height: '100%',
+            width: '100%',
+            backgroundColor: 'rgba(255,255, 255, 0.5)',
+            backdropFilter: 'blur(5px)',
+            position: 'absolute',
+            zIndex: '5000',
+            top: '0',
+            transition: '0.3s ease-in',
+          }}
+        >
+          <FullScreenLoader />
+        </Box>
+      ) : (
+        <>
+          {Array.isArray(data?.data) && data?.data.length !== 0 ? (
+            <CardsGridContainer container>
+              {data?.data.map(
+                ({
+                  id,
+                  attributes: {
+                    name,
+                    price,
+                    images: { data: imagesData },
+                    gender: { data: genderData },
+                  },
+                }: AttrFromData) => (
+                  <Grid item key={id}>
+                    <Card
+                      productCategory={
+                        genderData ? (genderData.id === 3 ? "Men's Shoes" : "Women's Shoes") : ''
+                      }
+                      productImageSrc={
+                        imagesData
+                          ? imagesData[0]
+                            ? imagesData[0].attributes.url
+                            : singInImg
                           : singInImg
-                        : singInImg
-                    }
-                    productName={name}
-                    productPrice={price}
-                  >
-                    <DropDownMenu productName={name} productID={id} />
-                  </Card>
-                </Grid>
-              )
-            )}
-          </CardsGridContainer>
-        ) : (
-          <CatalogIsEmptyContainer>
-            <Box
-              component={Image}
-              src={emptyIcon}
-              alt="catalog is empty"
-              width={queryDownMd ? 150 : 200}
-              height={queryDownMd ? 150 : 200}
-              priority={true}
-              sx={{
-                opacity: '0.1',
-              }}
-            />
-            <Typography
-              variant="h2"
-              sx={{ opacity: '0.5', width: queryDownMd ? '250px' : '375px' }}
-            >
-              Catalog is empty.
-            </Typography>
-          </CatalogIsEmptyContainer>
-        )}
-      </>
+                      }
+                      productName={name}
+                      productPrice={price}
+                    >
+                      <DropDownMenu productName={name} productID={id} />
+                    </Card>
+                  </Grid>
+                )
+              )}
+            </CardsGridContainer>
+          ) : (
+            <CatalogIsEmptyContainer>
+              <Box
+                component={Image}
+                src={emptyIcon}
+                alt="catalog is empty"
+                width={queryDownMd ? 150 : 200}
+                height={queryDownMd ? 150 : 200}
+                priority={true}
+                sx={{
+                  opacity: '0.1',
+                }}
+              />
+              <Typography
+                variant="h2"
+                sx={{ opacity: '0.5', width: queryDownMd ? '250px' : '375px' }}
+              >
+                Catalog is empty.
+              </Typography>
+            </CatalogIsEmptyContainer>
+          )}
+        </>
+      )}
     </CustomSearchOverlay>
   );
 };
